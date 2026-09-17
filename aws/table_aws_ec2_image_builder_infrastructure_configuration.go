f9d29d5eac794baadbc8c7bc5c412e77eabb1ecd
NorbertHambuer <norbert@aikido.dev>
2026-09-17 11:55:13 +0300

    fix: Sync only supported ec2 regions

--- aws/table_aws_ec2_image_builder_infrastructure_configuration.go
+++ aws/table_aws_ec2_image_builder_infrastructure_configuration.go
@@ -14,7 +14,7 @@ import (
 
 func tableAwsEc2ImageBuilderInfrastructureConfiguration(_ context.Context) *plugin.Table {
 	return &plugin.Table{
-		GetMatrixItemFunc: AllRegionsMatrix,
+		GetMatrixItemFunc: SupportedRegionMatrix(AWS_EC2_SERVICE_ID),
 		Name:              "aws_ec2_image_builder_infrastructure_configuration",
 		Description:       "AWS EC2 Image Builder Infrastructure Configuration",
 		Get: &plugin.GetConfig{
